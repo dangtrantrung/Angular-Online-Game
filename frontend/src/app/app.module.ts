f0c0a05dfc5a9b0f94389736bca21c5d0291f08f
dangtrantrung <trung280584@gmail.com>
2024-06-22 12:08:28 +0700

    Router Module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { HttpModule } from '@angular/http'
 
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { LobbyComponent } from './lobby/lobby.component';
+import { GameComponent } from './game/game.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LobbyComponent, GameComponent],
   imports: [BrowserModule, FormsModule, HttpModule],
   providers: [],
   bootstrap: [AppComponent],
