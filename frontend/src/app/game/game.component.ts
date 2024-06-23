0ea6de17657f21cda42e923d9286f248976618f4
dangtrantrung <trung280584@gmail.com>
2024-06-23 10:55:00 +0700

    game component with BS5

--- frontend/src/app/game/game.component.ts
+++ frontend/src/app/game/game.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+// import { MatSnackBar } from '@angular/material/snack-bar'
+import { ActivatedRoute } from '@angular/router'
+// import { SocketioService } from 'src/app/services/socketio.service';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: ['./game.component.css'],
 })
 export class GameComponent implements OnInit {
+  // gameId: string
+  role = 'operative'
+  // words
 
-  constructor() { }
+  constructor() // private route: ActivatedRoute, // private socketIoService: SocketioService,
+  // private snackbar: MatSnackBar,
+  {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    // this.gameId = this.route.snapshot.paramMap.get('id');
+    // this.socketIoService.connect(this.gameId);
+    // this.recieveJoinedPlayers()
+    // this.recieveStartGame()
+    // this.recieveGameUpdate()
   }
 
+  // nextGame() {
+  //   this.socketIoService.startGame(this.gameId);
+  // }
+
+  // startGame() {
+  //   this.socketIoService.startGame(this.gameId);
+  // }
+
+  // clickWord(word) {
+  //   word.selected = true;
+  //   this.socketIoService.sendGameUpdate(this.gameId, this.words);
+  // }
+
+  // recieveJoinedPlayers() {
+  //   this.socketIoService.recieveJoinedPlayers().subscribe((message: string) => {
+  //     this.snackbar.open(message, '', {
+  //       duration: 3000,
+  //     });
+  //   });
+  // }
+
+  // recieveStartGame() {
+  //   this.socketIoService.recieveStartGame().subscribe((words) => {
+  //     this.role = 'operative';
+  //     this.words = words;
+  //   });
+  // }
+
+  // recieveGameUpdate() {
+  //   this.socketIoService.recieveGameUpdate(this.gameId).subscribe((words) => {
+  //     this.words = words;
+  //   });
+  // }
 }
