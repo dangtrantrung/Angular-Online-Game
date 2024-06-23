4038de67d4652b96c1857e82ca23b5444318c06a
dangtrantrung <trung280584@gmail.com>
2024-06-23 15:22:33 +0700

    allowEIO3: true , socket.io

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
-var test = "test"
-console.log(test)
-const app = require('express')
-const httpServer = require('http').createServer(app)
+const app = require('express');
+const httpServer = require('http').createServer(app);
 const io = require('socket.io')(httpServer, {
     cors: true,
-    origins: ['*']
-})
-io.on('connection', (socket) => {
-    console.log('a user connected');
+    origins: ["*"],
+    allowEIO3: true
+});
+
+// const { createGame } = require('./util/words');
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
 })
 const PORT = process.env.PORT || 3000;
 httpServer.listen(PORT, () => {
