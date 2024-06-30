cf94d0e2db49a087c0ee30c6ba43b6dd1088e32c
dangtrantrung <trung280584@gmail.com>
2024-06-30 08:58:24 +0700

    recieveJoinedPlayers

--- backend/index.js
+++ backend/index.js
@@ -6,25 +6,29 @@ const io = require('socket.io')(httpServer, {
     allowEIO3: true
 });
 
-// const { createGame } = require('./util/words');
+const { createGame } = require('./util/words');
 
 io.on("connection", (socket) => {
     console.log("a user connected");
-    //socket emit
-    // socket.emit('message', 'Hey i just connected')
-    // socket.broadcast.emit('message', 'hi this message is send to everyone except sender')
-    // io.emit('this is send to everyone')
-    // socket.join('here is a unique id for the room')
-    // socket.to('room unique id').emit('message', 'this message is send to everyone in the room except the sender')
-    // io.to('room unique id').emit('message', 'this message is send to everyone in the room')
+
+    socket.on('startGame', ({ gameId }) => {
+        createGame().then(words => {
+            io.to(gameId).emit('startGame', words);
+            console.log("Someone is starting a game");
+        })
+    })
+
+    socket.on('gameUpdate', ({ gameId, words }) => {
+        io.to(gameId).emit(gameId, words);
+    })
+
     socket.on('joinGame', ({ gameId }) => {
         socket.join(gameId);
         console.log("a player joined the room " + gameId);
         socket.to(gameId).emit('joinGame', "A player joined the game!");
     })
+});
 
-})
 const PORT = process.env.PORT || 3000;
-httpServer.listen(PORT, () => {
-    console.log('Server is listenning on Port: ', PORT)
-})
+
+httpServer.listen(PORT, () => console.log('Server is running on port ' + PORT));
--- frontend/src/app/services/socketio.service.ts
+++ frontend/src/app/services/socketio.service.ts
@@ -1,18 +1,50 @@
 import { Injectable } from '@angular/core'
-import { environment } from 'environments/environment'
-
+import { environment } from 'src/environments/environment'
 import { io, Socket } from 'socket.io-client'
+import { Observable } from 'rxjs'
+import { ObserversModule } from '@angular/cdk/observers'
+
 @Injectable()
+// providedIn: 'root',
 export class SocketioService {
   socket: Socket
 
   constructor() {}
 
   connect(gameId) {
-    // this.socket = io(environment.SOCKET_ENDPOINT)
-    // this.socket.emit('joinGame', { gameId: gameId })
     this.socket = io(environment.SOCKET_ENDPOINT)
     this.socket.emit('joinGame', { gameId: gameId })
-    console.log('socket.io work')
+  }
+
+  startGame(gameId) {
+    this.socket.emit('startGame', { gameId: gameId })
+  }
+
+  sendGameUpdate(gameId, words) {
+    this.socket.emit('gameUpdate', { gameId: gameId, words: words })
+  }
+
+  recieveJoinedPlayers() {
+    return new Observable((observer) => {
+      this.socket.on('joinGame', (message) => {
+        observer.next(message)
+      })
+    })
+  }
+
+  recieveStartGame() {
+    return new Observable((observer) => {
+      this.socket.on('startGame', (words) => {
+        observer.next(words)
+      })
+    })
+  }
+
+  recieveGameUpdate(gameId) {
+    return new Observable((observer) => {
+      this.socket.on(gameId, (words) => {
+        observer.next(words)
+      })
+    })
   }
 }
